1ef00757fcfda2cb4d111aa013db41488cc646dc
Ehsan Hosseini <Hoseini.ehsan2007@gmail.com>
2019-12-11 14:11:51 +0100

    make app

---
+++ src/app_REST_API_to_MySQL.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+"""
+Created on Wed Dec 11 14:05:47 2019
+
+@author: Ehsan Hosseini
+
+Application of REST API and MSQL DB 
+"""
+# built-in modules
+
+
+
+from src.REST_API import RESTAPI
+from src.db_connect import DbConnection
+
+
+
